9690c63edd4a86071418cbd23c9b06fe6d7703e3
portilloj6-debug <portilloj6@montclair.edu>
2026-04-08 18:35:45 -0400

    Update Item.swift

--- Item.swift
+++ Item.swift
@@ -14,10 +14,25 @@ final class Item {
     var text: String = ""
     var pinned: Bool = false
     var folder: Folder?
+    var isBold: Bool = false
+    var textSize: Double = 18
+    var highlightColorName: String = "clear"
+    var fontName: String = "system"
 
-    init(timestamp: Date, text: String = "") {
+    init(timestamp: Date, text: String = "",
+        pinned: Bool = false,
+        folder: Folder? = nil,
+        isBold: Bool = false,
+        textSize: Double = 18,
+        highlightColorName: String = "clear",
+        fontName: String = "system") {
         self.timestamp = timestamp
         self.text = text
         self.pinned = false
+        self.folder = folder
+        self.isBold = isBold
+        self.textSize = textSize
+        self.highlightColorName = highlightColorName
+        self.fontName = fontName
     }
 }
