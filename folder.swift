7e841fea0b8bed872bc2fedd674a28f950c3e44e
Imtehan <45443247+ImtehanK@users.noreply.github.com>
2026-04-07 22:14:34 -0400

    Create folder.swift

---
+++ folder.swift
@@ -0,0 +1,14 @@
+import Foundation
+import SwiftData
+
+@Model
+final class Folder {
+    var name: String
+    @Relationship(deleteRule: .nullify, inverse: \Item.folder) var items: [Item] = []
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+
