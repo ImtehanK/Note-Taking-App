089bbfa401f3c0382634d2564bd55fff74ae3ddb
Jeanpierre22 <vidaljeanpierre22@gmail.com>
2026-05-02 23:05:18 -0400

    Update Item.swift

--- Item.swift
+++ Item.swift
@@ -4,16 +4,26 @@ import SwiftData
 @Model
 final class Item {
     var timestamp: Date
-    var text: String = ""
+    var text: AttributedString = AttributedString("")
     var pinned: Bool = false
     var folder: Folder?
     var colorName: String = NoteColor.allCases.randomElement()!.rawValue
 
-    init(timestamp: Date, text: String = "") {
+    var fontName: String = "system"
+    var textSize: Double = 16
+    var isBold: Bool = false
+    var highlightColorName: String = "clear"
+
+    init(timestamp: Date, text: AttributedString = AttributedString("")) {
         self.timestamp = timestamp
         self.text = text
         self.pinned = false
         self.colorName = NoteColor.allCases.randomElement()!.rawValue
+
+        self.fontName = "system"
+        self.textSize = 16
+        self.isBold = false
+        self.highlightColorName = "clear"
     }
 
     var noteColor: NoteColor {
